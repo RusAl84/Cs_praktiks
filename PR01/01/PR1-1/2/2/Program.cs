b2f7e2aa27fc9ddbdd1ae2f1649563b6f900ce71
rusal84 <rusal@bk.ru>
2020-09-03 15:37:42 +0300

    PR1-1-2

---
+++ PR01/01/PR1-1/2/2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PR1_1
+{
+  public enum Genders : int { Male, Female };
+  struct Person
+  {
+    public string firstName;
+    public string lastName;
+    public int age;
+    public Genders gender;
+    public Person(string _firstName, string _lastName, int _age, Genders _gender)
+    {
+      firstName = _firstName;
+      lastName = _lastName; age = _age;
+      gender = _gender;
+    }
+    public override string ToString()
+    {
+      return firstName + " " + lastName + " (" + gender + "), age " + age;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person p = new Person("Rusakov", "Aleksey", 35, Genders.Male);
+      Console.WriteLine(p.ToString());
+
+      Console.ReadKey();
+    }
+  }
+}
