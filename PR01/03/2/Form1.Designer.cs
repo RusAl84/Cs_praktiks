70d2e55e5e7fa22462201e5dd52fbee86f502949
rusal84 <rusal@bk.ru>
2020-09-03 17:34:57 +0300

    that\s all

---
+++ PR01/03/2/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace _2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.trackBar1 = new System.Windows.Forms.TrackBar();
+      this.progressBar3 = new System.Windows.Forms.ProgressBar();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(22, 12);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(753, 204);
+      this.progressBar1.TabIndex = 0;
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // trackBar1
+      // 
+      this.trackBar1.Location = new System.Drawing.Point(22, 248);
+      this.trackBar1.Maximum = 100;
+      this.trackBar1.Name = "trackBar1";
+      this.trackBar1.Size = new System.Drawing.Size(753, 45);
+      this.trackBar1.TabIndex = 1;
+      this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+      // 
+      // progressBar3
+      // 
+      this.progressBar3.Location = new System.Drawing.Point(22, 300);
+      this.progressBar3.Name = "progressBar3";
+      this.progressBar3.Size = new System.Drawing.Size(753, 138);
+      this.progressBar3.TabIndex = 3;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.progressBar3);
+      this.Controls.Add(this.trackBar1);
+      this.Controls.Add(this.progressBar1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ProgressBar progressBar3;
+    }
+}
+
---
+++ PR01/03/2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          if (progressBar1.Value > 99)
+         {
+            progressBar1.Value = 0;
+          }
+
+         progressBar1.Value += 1;
+         trackBar1.Value=progressBar1.Value;
+         progressBar3.Value=100- progressBar1.Value;
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void trackBar1_ValueChanged(object sender, EventArgs e)
+    {
+      progressBar1.Value = trackBar1.Value;
+    }
+  }
+}
