ad4d563ee3b3080550d882243282e581826d3a02
rusal84 <rusal@bk.ru>
2020-09-03 16:30:37 +0300

    2-1

---
+++ PR01/02/1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // C#
+      SByte a = 0; 
+      Byte b = 0; 
+      Int16 c = 0; 
+      Int32 d = 0; 
+      Int64 e = 0; 
+      string s = "";
+      Exception ex = new Exception();
+      // C#
+      object[] types = { a, b, c, d, e, s, ex };
+      // C#
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
+
+
+    }
+  }
+}
