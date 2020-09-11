c6b9de8c906aebb14d60b1a16538425172e2ec1c
RusAl <teacher@C320U00>
2020-09-11 12:00:37 +0300

    add upr1 file 3 (pr1)

---
+++ praktika1/03/1/CreateStruct/CreateStruct/Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+            Console.WriteLine(p.ToString());
+
+            Manager m = new Manager("Tony", "Allen", 32, Manager.Genders.Male, "555-555-1212", "123b");
+            Console.WriteLine(m);
+        }
+
+        class Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public Genders gender;
+
+            public Person(string _firstName, string _lastName, int _age, Genders _gender)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+                gender = _gender;
+            }
+
+            public override string ToString()
+            {
+                return firstName + " " + lastName + " (" + gender + "), age " + age;
+            }
+
+            public enum Genders : int { Male, Female };
+        }
+        class Manager : Person
+        {
+            string phoneNumber;
+            string officeLocation;
+
+            public Manager(string _firstName, string _lastName, int _age, Genders _gender, string _phoneNumber, string _officeLocation) 
+                : base (_firstName, _lastName, _age, _gender)
+            {
+                phoneNumber = _phoneNumber;
+                officeLocation = _officeLocation;
+            }
+
+            public override string ToString()
+            {
+                return base.ToString() + ", " + phoneNumber + ", " + officeLocation;
+            }
+        }
+    }
+}
