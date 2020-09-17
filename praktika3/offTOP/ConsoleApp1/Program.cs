f9b3e53f9f87ce1e2b95414447883ae3dd965d36
rusal <teacher@C246U00>
2020-09-17 16:40:56 +0300

    add offTOP sample regex

---
+++ praktika3/offTOP/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] text ={"Макс",
+        "C++",
+        " +79263772622",
+        " 89263772622",
+        " (926)3772622" };
+
+      foreach (string item in text)
+      {
+        string pattern = @"\d{7}";
+        if (Regex.IsMatch(item, pattern))
+        {
+          //Console.WriteLine($"{item} это телефон"); 
+          Console.WriteLine(item +" это телефон");
+        }
+        else
+        {
+            Console.WriteLine(
+            string.Format("{0} это НЕ телефон", item)
+            );
+        }
+
+      }
+      
+    }
+  }
+}
