c388fc67d3aba004af31310708afc984a903ec78
rusal <teacher@C246U00>
2020-09-17 16:48:27 +0300

    add praktika3 01 upr2

---
+++ praktika3/01/pr3-01-upr2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace pr3_01_upr2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamReader sr= new StreamReader(@"boot.ini");
+      StreamWriter sw = new StreamWriter(@"boot - utf7.txt", false, Encoding.UTF7);
+      sw.WriteLine(sr.ReadToEnd());
+      sw.Close();
+      sr.Close();
+
+    }
+  }
+}
