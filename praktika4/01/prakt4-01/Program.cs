79bd8f614aef30bc1db130493b747c6da780c495
rusal <teacher@C246U00>
2020-09-17 17:36:39 +0300

    add prakt4 samples

---
+++ praktika4/01/prakt4-01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace prakt4_01
+{
+  class Program
+  {
+
+      static void Main(string[] args)
+      {
+        ArrayList myList = new ArrayList();
+        myList.Add("First");
+        myList.Add("Second");
+        myList.Add("Third");
+        myList.Add("Fourth");
+        foreach (string item in myList)
+        {
+          Console.WriteLine("Unsorted: {0}", item);
+        }
+        // Сортировка при помощи стандартного объекта сравнения 
+        myList.Sort();
+        foreach (string item in myList)
+        {
+          Console.WriteLine("   Sorted: {0}", item);
+        }
+
+      }
+
+  }
+}
---
+++ praktika4/02/prak4-02-upr01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace prak4_02_upr01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();
+
+      queue.Enqueue("МАКС");
+      queue.Enqueue("F11");
+      queue.Enqueue("LOVE C++");
+      queue.Enqueue("***");
+
+      while (queue.Count > 0)
+      {
+        object obj = queue.Dequeue();
+        Console.WriteLine("From Queue: {0}", obj);
+      }
+    }
+  }
+}
