e9910ece4b717ce313dfb36b58734fe5b0f815a5
rusal <teacher@C246U00>
2020-09-17 17:38:38 +0300

    stack

---
+++ praktika4/02/prak4-02-upr02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace prak4_02_upr02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Stack stack = new Stack();
+      stack.Push("FIL");
+      stack.Push("MERS");
+      stack.Push("TRUB");
+      stack.Push("Hokkey");
+      while (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("'From Stack: {0}", obj);
+      }
+
+    }
+  }
+}
