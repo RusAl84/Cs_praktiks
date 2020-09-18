36a84489833ab5528fbaeddfa2157ac94774a395
Rusakov Aleksey <rusal@bk.ru>
2020-09-18 20:45:05 +0300

    fix pr1 3

--- praktika1/02/1/Program.cs
+++ praktika1/02/1/Program.cs
@@ -10,13 +10,11 @@ namespace _1
       SByte a = 0; 
       Byte b = 0; 
       Int16 c = 0; 
-      Int32 d = 0; 
-      Int64 e = 0; 
+      Int32 d = 0;
+      System.Int64 e = 0; 
       string s = "";
-      Exception ex = new Exception();
-      // C#
+      Exception ex = new Exception(); 
       object[] types = { a, b, c, d, e, s, ex };
-      // C#
       foreach (object o in types)
       {
         string type;
@@ -25,8 +23,6 @@ namespace _1
           type = "Reference Type";
         Console.WriteLine("{0}: {1}", o.GetType(), type);
       }
-
-
     }
   }
 }
