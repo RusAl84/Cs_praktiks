464a17e885a01cefc0bae74e7ed18d3bcaa0d195
Rusakov Aleksey <rusal@bk.ru>
2020-09-18 20:16:57 +0300

    fix pr1 1

--- praktika1/01/up1/PR01-1-1/Program.cs
+++ praktika1/01/up1/PR01-1-1/Program.cs
@@ -2,31 +2,46 @@
 
 namespace PR1_1
 {
-    struct Person
+    class Person
     {
         public string firstName;
         public string lastName;
+        static public string sekret;
         public int age;
 
-        public Person(string _firstName, string _lastName, int _age)
+
+        public Person(string _firstName, string _lastName, int _age, string _sekret)
         {
             firstName = _firstName;
-            lastName = _lastName; age = _age;
+            lastName = _lastName; 
+            age = _age;
+            sekret = _sekret;
         }
+        public Person(string _firstName, string _lastName)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = 18;
+            sekret = "MARS";
+
+        }
+
         public override string ToString()
         {
-            return firstName + " " + lastName + ", age " + age;
+            return firstName + " " + lastName + ", age " + age+ ", sekret " + sekret;
         }
-
-
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Person p = new Person("Rusakov", "Alexey", 35);
-            Console.WriteLine(p.ToString());
+            Person p = new Person("Rusakov", "Alexey", 35,"sekret_fvv");
+            Person p1 = new Person("Rusakov", "Alexey");
+            Person p2 = new Person("Rusakov", "Alexey", 35, "global 2GB");
+            Console.WriteLine(p);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
             Console.ReadKey();
         }
     }
---
+++ praktika2/03/Compression/Compression/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inFilename = @"d:\ttt.txt";
+            string outFilename = @"d:\ttt.txt.gz";
+            //FileStream sourceFile = File.OpenRead(inFilename);
+            //FileStream destFile = File.Create(outFilename);
+
+            //GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            //int theByte = sourceFile.ReadByte();
+            //while (theByte != -1)
+            //{
+            //    compStream.WriteByte((byte)theByte);
+            //    theByte = sourceFile.ReadByte();
+            //}
+            //compStream.Close();
+
+
+            string inFilename1 = @"d:\tttt.txt";
+            FileStream sourceFile = File.OpenRead(outFilename);
+            FileStream destFile = File.Create(inFilename1);
+            GZipStream compStream =
+            new GZipStream(sourceFile, CompressionMode.Decompress);
+            int theByte = compStream.ReadByte();
+            while (theByte != -1)
+            {
+                destFile.WriteByte((byte)theByte);
+                theByte = compStream.ReadByte();
+            }
+
+            compStream.Close();
+
+
+        }
+    }
+}
