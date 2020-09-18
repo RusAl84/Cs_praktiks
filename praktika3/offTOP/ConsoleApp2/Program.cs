0d1a12947d66f67bdb553d2536cb575f2c0cb177
teacher <teacher@C239U00>
2020-09-18 14:15:48 +0300

    add praktika 3 offtop

---
+++ praktika3/offTOP/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string file_name = @"D:\Documents\1.txt";
+      Console.WriteLine(file_name);
+      StreamWriter file = new StreamWriter(file_name);
+      for (int i = 0; i < 1000; i++)
+      {
+        file.WriteLine($"Evgen Contex Vazelin {i} ml ");
+      }
+      file.Close();
+      Console.WriteLine("Считать файл?");
+      Console.ReadLine();
+      StreamReader rfile= new StreamReader(file_name);
+      for (int i = 0; i < 1000; i++)
+      {
+        Console.WriteLine(rfile.ReadLine());
+      }
+
+    }
+  }
+}
---
+++ ЗАДАНИЯ/Практика 2/Code Практикум/Lesson1/Before/Exercise1/CS/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    
+  }
+}
