14f5a15f41770e942045006ee9fab78b77bdfa8f
teacher <teacher@C239U00>
2020-09-18 14:29:31 +0300

    add pr2

---
+++ praktika2/01/pr2-01-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace pr2_01_02
+{
+  class Program
+  {// C#
+    static void watcher_Changed(object sender, FileSystemEventArgs e)
+    {
+      Console.WriteLine("Changed: {0}", e.FullPath);
+    }
+
+    static void Main(string[] args)
+    {
+      // C#
+      FileSystemWatcher watcher =
+      new FileSystemWatcher(@"D:\Documents");
+
+      // C#
+      watcher.Filter = "*.*";
+      watcher.IncludeSubdirectories = true;
+      watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+      watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+      Console.ReadKey();
+
+
+    }
+  }
+}
