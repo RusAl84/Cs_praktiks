b59e8f361d3b3b625b5d9f07038ee4caf73c5b6a
teacher <teacher@C239U00>
2020-09-18 14:22:32 +0300

    add pr2

---
+++ praktika2/01/pr2-01-01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace pr2_01_01
+{
+  class Program
+  {
+    static void ShowDirectory(DirectoryInfo dir)
+    {
+      // Показать все файлы
+      foreach (FileInfo file in dir.GetFiles())
+      {
+        Console.WriteLine("File: {0}", file.FullName);
+      }
+
+      foreach (DirectoryInfo subDir in dir.GetDirectories())
+      {
+        ShowDirectory(subDir);
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      // C#
+      DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory); 
+      ShowDirectory(dir);
+    }
+  }
+}
