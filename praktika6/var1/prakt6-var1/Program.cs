b83844de69deeb55d6915b7f7d695012b1dabeea
teacher <teacher@C239U00>
2020-10-02 15:43:39 +0300

    add pr6

---
+++ praktika6/var1/prakt6-var1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace prakt6_var1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //person Sasha_VDV = new person("Sasha","Kozel", DateTime.Parse("27.12.1984",
+      //                    "");
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
