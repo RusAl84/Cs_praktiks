43ea3005348bae1687ff0fd7949b350c4de56b86
Rusakov Aleksey <rusal@bk.ru>
2020-10-16 14:28:53 +0300

    add CLient Server API

---
+++ praktika7/offTop/API server/API-server/API-server.sln/API server/Controllers/mireaController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mireaController : ControllerBase
+    {
+        // GET: api/<mireaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Андрюша", "поехал в Сирию" };
+        }
+
+        // GET api/<mireaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string str1 = "Поедешь в Сирию";
+            if (id > 500){
+                str1 = "Всё будет хорошо";
+            }
+            else
+            if (id > 10000)
+            {
+                str1 = "Военный билет c доставкой на дом";
+            }
+            return str1;
+        }
+
+        // POST api/<mireaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<mireaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<mireaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ praktika7/offTop/ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер пожертвования:");
+            string str1 = Console.ReadLine();
+            System.Net.WebRequest reqGET = System.Net.WebRequest.Create(@"http://localhost:5000/api/mirea/"+str1);
+            System.Net.WebResponse resp = reqGET.GetResponse();
+            System.IO.Stream stream = resp.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            string s = sr.ReadToEnd();
+            Console.WriteLine(s);
+        }
+
+        static async System.Threading.Tasks.Task GetDataAsync(int i)
+        {
+            //WebRequest request = WebRequest.Create("http://localhost:5374/Home/PostData?sName=Иван Иванов&age=31");
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/mirea/500");
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
+    }
+}
