cb95e5a320cc06eec1c96ac290cd3e169519333e
Rusakov Aleksey <rusal@bk.ru>
2020-10-20 15:38:27 +0300

    1

---
+++ praktika8/02/SingleInstance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const String MutexName = "RUNMEONLYONCE";
+            try // Пытаемся открыть Mutex
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                // He можем открыть Mutex, потому что он не существует
+            }
+            // Создаем его, если он не существует 
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else
+            {
+                // Закрываем Mutex и выходим из приложения,
+                // так как разрешается запуск только одного его экземпляра
+                oneMutex.Close();
+                return;
+            }
+            Console.WriteLine("Our Application");
+            Console.Read();
+        }
+    }
+}
