bef738e0838469f4e3c08d485414fef33ecd7f1d
Rusakov Aleksey <rusal@bk.ru>
2020-10-20 13:57:54 +0300

    jkulvich add game engine!!!

--- praktika7/offTop/Risovat_na_Canvas/Form1.Designer.cs
+++ praktika7/offTop/Risovat_na_Canvas/Form1.Designer.cs
@@ -28,37 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(714, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(221, 119);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.PRO_C__7;
-            this.pictureBox1.Location = new System.Drawing.Point(29, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(599, 584);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1250, 650);
+            this.ClientSize = new System.Drawing.Size(625, 548);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -67,9 +65,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- praktika7/offTop/Risovat_na_Canvas/Form1.cs
+++ praktika7/offTop/Risovat_na_Canvas/Form1.cs
@@ -11,41 +11,146 @@ using System.Drawing;
 
 namespace WindowsFormsApp1
 {
+    public class Shot {
+        public int lifeTime;
+        public Point pos;
+
+        public Shot(Point pos) {
+            this.pos = pos;
+            this.lifeTime = 30;
+        }
+    }
+
     public partial class Form1 : Form
     {
         public int x = 0;
+        public int x1 = 0;   
+        public int y1 = 0;
+
+        public int gameTime = 0;
+
+        // Пути к ресурсам спрайтов
+        string girlImagePath = "../../Resources/girl.jpg";
+        string tankImagePath = "../../Resources/tank.png";
+
+        // Ресурсы игры
+        Bitmap girlImage;
+        Bitmap tankImage;
+        Pen aquaPen;
+        Font fontMain;
+        Random rand;
+
+        // Экраны
+        Bitmap screen;
+
+        // Инструменты
+        Graphics g;
+
+        // Состояние игры
+        List<Shot> shots = new List<Shot>();
+
         public Form1()
         {
             InitializeComponent();
+
+            this.Width = 400;
+            this.Height = 400;
+            pictureBox1.Top = 0;
+            pictureBox1.Left = 0;
+            pictureBox1.Width = this.Width;
+            pictureBox1.Height = this.Height;
+
+            // Загрузка ресурсов
+            girlImage = new Bitmap(girlImagePath);
+            tankImage = new Bitmap(tankImagePath);
+            aquaPen = new Pen(Color.Aqua, 3);
+            aquaPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            aquaPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            fontMain = new Font(FontFamily.GenericSansSerif, 15);
+            rand = new Random();
+
+            // Инициализация экранов
+            screen = new Bitmap(400, 400);
+
+            // Инициализация инструментов
+            g = Graphics.FromImage(screen);
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+
+            // Настройка компонентов
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            // Stretches the image to fit the pictureBox.
-            Bitmap MyImage;
-            string fileToDisplay = @"D:\temp\1.jpg";
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-
-            MyImage = new Bitmap(fileToDisplay);
-
             // Create pen.
-            Pen blackPen = new Pen(Color.Black, 30);
-            // Create coordinates of points that define line.
-            x += 5;
-            int x1 = 1 + x;   //topleft to topright
-            int y1 = 1 + x;
-            int x2 = 300 + x + (int)(1 * x);
-            int y2 = 300 + x + (int)(1 * x);
+            //Pen blackPen = new Pen(Color.Aqua, 10);
+            //DrawHair(blackPen);
 
-            // Draw line to screen.
-            using (var graphics = Graphics.FromImage(MyImage))
-            {
-                graphics.DrawLine(blackPen, x1, y1, x2, y2);
+            // Очистка экрана и отрисовка девочки
+            g.Clear(Color.White);
+            g.DrawImage(girlImage, 0, 0, screen.Width, screen.Height);
+
+            // Отрисовка волос
+            for (var i = 0; i < 30; i++) {
+                int hairOffsetX = 150;
+                int hairOffsetY = 70;
+                g.DrawLine(
+                    aquaPen, 
+                    hairOffsetX + i * 5, 
+                    hairOffsetY, 
+                    hairOffsetX + i * 5 + (int)(Math.Sin(gameTime / 8.0) * 8.0), 
+                    hairOffsetY - 50
+                );
             }
 
+            // Отрисовка танка            
+            var tankPos = new Point(
+                (int)(Math.Sin(gameTime / 10.0) * 100.0 + 200.0), 
+                (int)(Math.Cos(gameTime / 15.0) * 100.0 + 200.0)
+            );            
+            g.DrawImage(
+                tankImage, 
+                tankPos.X,
+                tankPos.Y,
+                50, 
+                50
+            );
 
-            pictureBox1.ClientSize = new Size(400, 400);
-            pictureBox1.Image = (Image)MyImage;
+            // Создание выстрела
+            if (gameTime % 10 == 0) {
+                shots.Add(new Shot(new Point(rand.Next(0, screen.Width), rand.Next(0, screen.Height))));
+            }
+
+            // Отрисовка выстрелов
+            foreach (var shot in shots) {
+                var radius = shot.lifeTime;
+                g.FillEllipse(
+                    new SolidBrush(Color.Red), 
+                    shot.pos.X - radius/2, 
+                    shot.pos.Y - radius/2, 
+                    radius, 
+                    radius
+                );                
+            }
+           
+            // Обработка жизненного цикла выстрелов
+            for (var i = 0; i < shots.Count; i++)
+            {
+                var shot = shots[i];
+                shot.lifeTime--;
+                if (shot.lifeTime <= 0) {
+                    shots.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            // Отрисовка текста
+            g.DrawString("Я люблю C#", fontMain, new SolidBrush(Color.Black), 0, 0);
+
+            // Обновление изображения на экране
+            pictureBox1.Image = screen;
+
+            gameTime++;
         }
     }
 }
