ca7f976bcefd1b61764b97eebf0abfe0067f314a
Rusal84 <rusal@bk.ru>
2020-11-12 11:18:32 +0300

    BISO GUI CHAT

--- lec11/BISO/BISO-chat/Form1.Designer.cs
+++ lec11/BISO/BISO-chat/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp3
+﻿namespace BISO_chat
 {
   partial class Form1
   {
@@ -31,53 +31,45 @@
       this.components = new System.ComponentModel.Container();
       this.button1 = new System.Windows.Forms.Button();
       this.textBox1 = new System.Windows.Forms.TextBox();
-      this.textBox2 = new System.Windows.Forms.TextBox();
-      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.listBox1 = new System.Windows.Forms.ListBox();
       this.label1 = new System.Windows.Forms.Label();
       this.timer1 = new System.Windows.Forms.Timer(this.components);
       this.SuspendLayout();
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(376, 113);
+      this.button1.Location = new System.Drawing.Point(164, 173);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.Size = new System.Drawing.Size(131, 40);
       this.button1.TabIndex = 0;
-      this.button1.Text = "=";
-      this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+      this.button1.Text = "Авторизация";
       this.button1.UseVisualStyleBackColor = true;
       this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(77, 113);
+      this.textBox1.Location = new System.Drawing.Point(22, 182);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(100, 22);
       this.textBox1.TabIndex = 1;
       // 
-      // textBox2
+      // listBox1
       // 
-      this.textBox2.Location = new System.Drawing.Point(222, 113);
-      this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(100, 22);
-      this.textBox2.TabIndex = 2;
-      // 
-      // textBox3
-      // 
-      this.textBox3.Location = new System.Drawing.Point(501, 113);
-      this.textBox3.Name = "textBox3";
-      this.textBox3.Size = new System.Drawing.Size(100, 22);
-      this.textBox3.TabIndex = 3;
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.ItemHeight = 16;
+      this.listBox1.Location = new System.Drawing.Point(22, 12);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(289, 148);
+      this.listBox1.TabIndex = 2;
       // 
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(189, 113);
+      this.label1.Location = new System.Drawing.Point(379, 27);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(16, 17);
-      this.label1.TabIndex = 4;
-      this.label1.Text = "+";
-      this.label1.Click += new System.EventHandler(this.label1_Click);
+      this.label1.Size = new System.Drawing.Size(46, 17);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "label1";
       // 
       // timer1
       // 
@@ -91,12 +83,12 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Controls.Add(this.label1);
-      this.Controls.Add(this.textBox3);
-      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.listBox1);
       this.Controls.Add(this.textBox1);
       this.Controls.Add(this.button1);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -106,8 +98,7 @@
 
     private System.Windows.Forms.Button button1;
     private System.Windows.Forms.TextBox textBox1;
-    private System.Windows.Forms.TextBox textBox2;
-    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.ListBox listBox1;
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Timer timer1;
   }
---
+++ lec11/BISO/BISO-chat/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BISO_chat
+{
+  public partial class Form1 : Form
+  {
+    public string authorization;
+    public Form2 form2;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //MessageBox.Show("Love BISO");
+      //Declare your new form
+      //Form2 form2 = new Form2();
+      //Show your new form
+      form2.Show();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      authorization = "нет авторизации";
+      form2 = new Form2();
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      authorization = form2.authorization;
+      label1.Text = authorization;
+    }
+  }
+}
