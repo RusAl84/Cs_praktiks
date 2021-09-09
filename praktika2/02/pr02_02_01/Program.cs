f9bda0ec151992c347c84815a3a17e1a30452078
Alexey Rusakov <rusal@bk.ru>
2021-09-09 09:50:01 +0300

    add pr02_02_01

---
+++ praktika2/02/pr02_02_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace pr02_02_01
+{
+  class Program
+  {
+    //    static void spam()
+    //    {
+    //;
+    //    }
+    static void Main(string[] args)
+    {
+      StreamWriter writer = File.CreateText(@"D:\temp\newfile" + DateTime.Now.ToString("h_mm_ss") + ".txt");
+      for (int i = 0; i < 100e6; i++)
+      {
+        writer.WriteLine("Глеб слушает Ваолерия Меладзе и ФАНАТЕЕТ;)");
+      }
+      writer.Close();
+     } 
+  }
+}
