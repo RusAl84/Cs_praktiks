7fb1ef9e2f9043b69e7aab3c33ec26b55b42d4d4
Alexey Rusakov <rusal@bk.ru>
2021-09-09 18:59:14 +0300

    fix

---
+++ KillComp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KillComp
+{
+  class Program
+  {
+    /*fdfdfddf
+     * args
+     * 
+     */
+    static void Main(string[] args)
+    {
+      for(int i = 0; i < 100; i++)
+      {
+        Console.Beep(10000 - 100 * i, 300 - 2 * i);
+      }
+      Console.Beep(2000, 30*1000);
+      
+    }
+  }
+}
