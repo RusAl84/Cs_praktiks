845ca2eba01c5058c1fab7fa0c9ac8f4dde44969
Alexey Rusakov <rusal@bk.ru>
2021-09-09 11:05:17 +0300

    add spam offtop

--- praktika2/02/pr02_02_01/Program.cs
+++ praktika2/02/pr02_02_01/Program.cs
@@ -5,18 +5,19 @@ namespace pr02_02_01
 {
   class Program
   {
-    //    static void spam()
-    //    {
-    //;
-    //    }
+    static void spam()
+    {
+      StreamWriter writer = File.CreateText("D:\\temp\\newfile" + DateTime.Now.ToString("h_mm_ss") + ".txt");
+      for (int i = 0; i< 100e6; i++)
+      {
+        writer.WriteLine("Сережа сбрил брови;)");
+      }
+    writer.Close();
+    }
     static void Main(string[] args)
     {
-      StreamWriter writer = File.CreateText(@"D:\temp\newfile" + DateTime.Now.ToString("h_mm_ss") + ".txt");
-      for (int i = 0; i < 100e6; i++)
-      {
-        writer.WriteLine("Глеб слушает Ваолерия Меладзе и ФАНАТЕЕТ;)");
-      }
-      writer.Close();
+      for (int i = 0; i < 10; i++)
+        spam();
      } 
   }
 }
