0cb80e4058ec2140c33cc8a35f2c945beb64a8bc
Alexey Rusakov <rusal@bk.ru>
2021-09-29 09:21:55 +0300

    Ezh sample

---
+++ praktika3/EzhSample/EzhSampleSer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EzhSampleSer
+{
+  class Ezh
+  {
+    public string name="Артем";
+
+    public override string ToString()
+    {
+      return this.name;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Ezh ezh1 = new Ezh();
+
+      Console.WriteLine(ezh1);
+    }
+  }
+}
