1b99628c5891077fd8353e45620965e7e2f186c2
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-10-14 12:15:44 +0300

    fix

---
+++ praktika7/offTop/risuemTank/risuemTankWF/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace risuemTankWF
+{
+  public partial class Form1 : Form
+  {
+    int posX = 1;
+    int posY = 1;
+
+    public static int Width = 700;
+    public static int Height = 700;
+    public static Rectangle rect = new Rectangle(1, 1, Width - 2, Height - 2);
+    public Pen _pen = new Pen(Color.Black, 5);
+    public Brush _brush = Brushes.Violet;
+    public Graphics g;
+    public Bitmap bm = new Bitmap(Width, Height);
+    public void drawEl(int lx, int ly, int rx, int ry)
+    {
+      Rectangle rect = new Rectangle(lx, ly, rx, ry);
+      g.DrawEllipse(_pen, rect);
+      g.FillEllipse(_brush, rect);
+    }
+    public void draw()
+    {
+      g.Clear(Color.White);
+      int lpos = 100;
+      drawEl(490 + posX - lpos, 250, 160, 20); // дуло
+      drawEl(250 + posX - lpos, 190, 260, 210); // башня
+      drawEl(100 + posX - lpos, 300, 550, 200); // ГусеницА
+      if (posX % 2 ==0)
+      _brush = Brushes.Magenta;
+      drawEl(140 + posX - lpos, 330, 140, 140); // Колесо 1
+      if (posX % 2 == 0)
+        _brush = Brushes.DarkOrange;
+      drawEl(290 + posX - lpos, 330, 140, 140); // Колесо 2
+      if (posX % 2 == 0)
+        _brush = Brushes.HotPink;
+      drawEl(440 + posX - lpos, 330, 140, 140); // Колесо 3
+      _brush = Brushes.Violet;
+
+      drawEl(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text), Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text));
+      pictureBox1.Image = bm;
+    }
+
+
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+    {
+      label1.Text = e.X.ToString();
+      label2.Text = e.Y.ToString();
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      draw();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      g = Graphics.FromImage(bm);
+      g.SmoothingMode = SmoothingMode.HighQuality;
+      draw();
+      SoundPlayer sp = new SoundPlayer();
+      sp.SoundLocation = "1.wav";
+      sp.Load();
+      sp.PlayLooping();
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      posX++;
+      posY++;
+      draw();
+    }
+  }
+}
---
+++ praktika7/offTop/testDrawCanvas/testDrawCanvasWF/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testDrawCanvasWF
+{
+  public partial class Form1 : Form
+  {
+
+
+
+    public static int Width = 700;
+    public static int Height = 700;
+    Bitmap bm = new Bitmap(Width, Height);
+    Graphics g;
+    List<Color> listColor = new List<Color>() { Color.FloralWhite, Color.Orange, Color.DarkBlue, Color.IndianRed, Color.Chocolate, Color.HotPink, Color.GreenYellow, Color.DarkViolet, Color.MediumPurple}; 
+    public void draw()
+    {
+
+      Pen p = new Pen(Color.Black, 5);
+      Rectangle rect = new Rectangle(1, 1, Width - 2, Height - 2);
+      Brush b = new LinearGradientBrush(rect, Color.Violet, Color.White, (float)45);
+      Pen blackPen = new Pen(Color.Black, 3);
+      Random rnd = new Random();
+
+
+      
+      Rectangle rect2 = new Rectangle(rnd.Next(700), rnd.Next(700), rnd.Next(200), rnd.Next(200));
+      g.DrawRectangle(blackPen, rect2);
+      g.FillRectangle(new SolidBrush(listColor[rnd.Next(listColor.Count)]), rect2);
+      Rectangle rect3 = new Rectangle(rnd.Next(700), rnd.Next(700), rnd.Next(200), rnd.Next(200));
+      g.DrawRectangle(blackPen, rect3);
+      g.FillRectangle(new SolidBrush(listColor[rnd.Next(listColor.Count)]), rect3);
+      Rectangle rect4 = new Rectangle(rnd.Next(700), rnd.Next(700), rnd.Next(200), rnd.Next(200));
+      g.DrawRectangle(blackPen, rect4);
+      g.FillRectangle(new SolidBrush(listColor[rnd.Next(listColor.Count)]), rect4);         
+      Rectangle rect5 = new Rectangle(rnd.Next(700), rnd.Next(700), rnd.Next(200), rnd.Next(200));
+      g.DrawEllipse(blackPen, rect5);
+      g.FillEllipse(new SolidBrush(listColor[rnd.Next(listColor.Count)]), rect5);
+
+      pictureBox1.Image = bm;
+    }
+
+    public Form1()
+    {
+      InitializeComponent();
+      listBox1.DrawMode = DrawMode.OwnerDrawVariable;
+      listBox1.Items.Clear();
+
+      foreach (Color item in listColor)
+      {
+        listBox1.Items.Add(item.Name);
+      }
+      listBox1.BackColor = Color.Thistle;
+      this.listBox1.DrawItem +=
+  new System.Windows.Forms.DrawItemEventHandler(
+  this.listBox1_DrawItem);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      g = Graphics.FromImage(bm);
+      g.SmoothingMode = SmoothingMode.HighQuality;
+      // расширенное окно для выбора цвета
+      colorDialog1.FullOpen = true;
+      // установка начального цвета для colorDialog
+      colorDialog1.Color = this.BackColor;
+
+
+
+
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      draw();
+    }
+
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+      if (checkBox1.Checked)
+      {
+        timer1.Enabled = true;
+      }
+      if (!checkBox1.Checked)
+      {
+        timer1.Enabled = false;
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+        return;
+      // установка цвета формы
+      this.BackColor = colorDialog1.Color;
+    }
+
+    private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+    {
+      // Перерисовываем фон всех элементов ListBox.  
+      e.DrawBackground();
+
+      // Создаем объект Brush.  
+      Brush myBrush = new SolidBrush(listColor[e.Index]);
+
+      // Определяем номер текущего элемента  
+      //switch (e.Index)
+      //{
+      //  case 0:
+      //    myBrush = Brushes.Red;
+      //    break;
+      //  case 1:
+      //    myBrush = Brushes.Green;
+      //    break;
+      //  case 2:
+      //    myBrush = Brushes.Blue;
+      //    break;
+      //  default:
+          
+      //    break;
+      //}
+
+      //Если необходимо, закрашиваем фон   
+      //активного элемента в новый цвет  
+      e.Graphics.FillRectangle(myBrush, e.Bounds);  
+
+      // Перерисовываем текст текущего элемента  
+      e.Graphics.DrawString(
+          ((ListBox)sender).Items[e.Index].ToString(),
+          e.Font, myBrush, e.Bounds,
+          StringFormat.GenericDefault);
+
+      // Если ListBox в фокусе, рисуем прямоугольник   
+      //вокруг активного элемента.  
+      e.DrawFocusRectangle();
+    }
+
+    }
+}
