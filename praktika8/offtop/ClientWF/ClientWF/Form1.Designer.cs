d057effdd51c09a749e6613a4499913a990bb0c0
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-10-20 12:15:41 +0300

    FIX

--- praktika8/offtop/ClientWF/ClientWF/Form1.Designer.cs
+++ praktika8/offtop/ClientWF/ClientWF/Form1.Designer.cs
@@ -29,10 +29,12 @@ namespace ClientWF
     /// </summary>
     private void InitializeComponent()
     {
+      this.components = new System.ComponentModel.Container();
       this.listBox1 = new System.Windows.Forms.ListBox();
-      this.textBox1 = new System.Windows.Forms.TextBox();
-      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.userNameTB = new System.Windows.Forms.TextBox();
+      this.messageTextTB = new System.Windows.Forms.TextBox();
       this.button1 = new System.Windows.Forms.Button();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
       this.SuspendLayout();
       // 
       // listBox1
@@ -44,28 +46,37 @@ namespace ClientWF
       this.listBox1.Size = new System.Drawing.Size(748, 344);
       this.listBox1.TabIndex = 0;
       // 
-      // textBox1
+      // userNameTB
       // 
-      this.textBox1.Location = new System.Drawing.Point(12, 383);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(246, 27);
-      this.textBox1.TabIndex = 1;
+      this.userNameTB.Location = new System.Drawing.Point(12, 372);
+      this.userNameTB.Name = "userNameTB";
+      this.userNameTB.Size = new System.Drawing.Size(246, 27);
+      this.userNameTB.TabIndex = 1;
+      this.userNameTB.Text = "Ilusha";
       // 
-      // textBox2
+      // messageTextTB
       // 
-      this.textBox2.Location = new System.Drawing.Point(12, 416);
-      this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(571, 27);
-      this.textBox2.TabIndex = 2;
+      this.messageTextTB.Location = new System.Drawing.Point(12, 416);
+      this.messageTextTB.Name = "messageTextTB";
+      this.messageTextTB.Size = new System.Drawing.Size(571, 27);
+      this.messageTextTB.TabIndex = 2;
+      this.messageTextTB.Text = "Любит есть Ёжиков";
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(602, 383);
+      this.button1.Location = new System.Drawing.Point(602, 378);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(158, 60);
       this.button1.TabIndex = 3;
       this.button1.Text = "button1";
       this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Interval = 1000;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
       // 
       // Form1
       // 
@@ -73,11 +84,12 @@ namespace ClientWF
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Controls.Add(this.button1);
-      this.Controls.Add(this.textBox2);
-      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.messageTextTB);
+      this.Controls.Add(this.userNameTB);
       this.Controls.Add(this.listBox1);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -86,9 +98,10 @@ namespace ClientWF
     #endregion
 
     private System.Windows.Forms.ListBox listBox1;
-    private System.Windows.Forms.TextBox textBox1;
-    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.TextBox userNameTB;
+    private System.Windows.Forms.TextBox messageTextTB;
     private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Timer timer1;
   }
 }
 
--- praktika8/offtop/ClientWF/ClientWF/Form1.cs
+++ praktika8/offtop/ClientWF/ClientWF/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MessangerLibrary;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +15,77 @@ namespace ClientWF
 {
   public partial class Form1 : Form
   {
+    public int pos;
+
     public Form1()
     {
       InitializeComponent();
     }
 
+    private void button1_Click(object sender, EventArgs e)
+    {
+      MessageClass mes = new MessageClass();
+      mes.userName = "SAHSA KOZHANAY KURTKA";
+      mes.messaageText = "SASHA SNEMI KURTKU";
+      var client = new RestClient("http://localhost:5000");
+      var request = new RestRequest("/api/SendMessage", Method.POST);
+      request.AddJsonBody(JsonConvert.SerializeObject(mes));
+      var res = client.Execute(request);
+      
+      //if (response.StatusCode == System.Net.HttpStatusCode.Created)
+      //{
+      //  string str1 = "";
+      //}
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      pos = 0;
+      listBox1.Items.Clear();
+      UpdateLB();
+    }
+    public void UpdateLB()
+    {
+      string jsonString = "";
+      while (jsonString != "\"Not found\"") { 
+      var client = new RestClient("http://localhost:5000");
+      var request = new RestRequest("api/GetMessages/"+pos.ToString(), Method.GET);
+      var res = client.Execute(request);
+      if (res.IsSuccessful)
+      {
+        //MessageBox.Show($"Успех: {res.Content}");
+        jsonString = res.Content;
+        if (jsonString != "\"Not found\"") {
+
+            jsonString = jsonString.Replace("\\", "");
+            MessageClass mes = new MessageClass();
+           mes = JsonConvert.DeserializeObject<MessageClass>(jsonString);
+
+        listBox1.Items.Add(mes);
+
+        //listBox1.Items.Add(jsonString);
+          pos++;
+        }
+      }
+        //else
+        //{
+        //  if (res.StatusCode == 0)
+        //  {
+        //    MessageBox.Show($"Ошибка: ошибка сети: {res.ErrorMessage}");
+        //  }
+        //  else
+        //  {
+        //    MessageBox.Show($"Ошибка: {(int)res.StatusCode} - {res.StatusDescription}");
+        //  }
+        //}
+        //MessageBox.Show(queryResult);
+
+      }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      UpdateLB();
+    }
   }
 }
