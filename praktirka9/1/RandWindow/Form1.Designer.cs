7374bd3cbb6083dc967d55837e18e0fb83b76c50
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-10-27 09:25:38 +0300

    fix

--- praktirka9/1/RandWindow/Form1.Designer.cs
+++ praktirka9/1/RandWindow/Form1.Designer.cs
@@ -39,17 +39,19 @@
       // 
       this.label1.AutoSize = true;
       this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-      this.label1.Location = new System.Drawing.Point(84, 33);
+      this.label1.Location = new System.Drawing.Point(112, 41);
+      this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(303, 25);
+      this.label1.Size = new System.Drawing.Size(555, 33);
       this.label1.TabIndex = 0;
-      this.label1.Text = "Вы хотите сдать Мерсова?";
+      this.label1.Text = "Вы хотите сдать Русакова автоматом?";
       // 
       // button2
       // 
-      this.button2.Location = new System.Drawing.Point(377, 106);
+      this.button2.Location = new System.Drawing.Point(503, 130);
+      this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(95, 23);
+      this.button2.Size = new System.Drawing.Size(127, 28);
       this.button2.TabIndex = 2;
       this.button2.Text = "Нет";
       this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(89, 106);
+      this.button1.Location = new System.Drawing.Point(119, 130);
+      this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(97, 23);
+      this.button1.Size = new System.Drawing.Size(129, 28);
       this.button1.TabIndex = 3;
       this.button1.Text = "Да, конечно!";
       this.button1.UseVisualStyleBackColor = true;
@@ -67,28 +70,31 @@
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(13, 13);
+      this.textBox1.Location = new System.Drawing.Point(17, 16);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(44, 20);
+      this.textBox1.Size = new System.Drawing.Size(57, 22);
       this.textBox1.TabIndex = 4;
       // 
       // textBox2
       // 
-      this.textBox2.Location = new System.Drawing.Point(13, 37);
+      this.textBox2.Location = new System.Drawing.Point(17, 46);
+      this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(44, 20);
+      this.textBox2.Size = new System.Drawing.Size(57, 22);
       this.textBox2.TabIndex = 5;
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(569, 179);
+      this.ClientSize = new System.Drawing.Size(759, 220);
       this.Controls.Add(this.textBox2);
       this.Controls.Add(this.textBox1);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.button2);
       this.Controls.Add(this.label1);
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.Name = "Form1";
       this.Text = "First Windows";
       this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
