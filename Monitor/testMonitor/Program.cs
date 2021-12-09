3c4968674fa71a965415e6aff10139dd8eec73bd
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-12-09 10:00:15 +0300

    add Monitor task

--- Monitor/testMonitor/Program.cs
+++ Monitor/testMonitor/Program.cs
@@ -4,12 +4,24 @@ namespace testMonitor
 {
     internal class Program
     {
-
-
-
+        /// <summary>
+        /// Задание
+        ///    1. Создайте консольное приложение на языке C# в среде MS Visual Studio для реализации монитора.
+        ///    2. Задайте свои параметры:
+        ///       * число процессов,
+        ///       * количество циклов вычисления случайных чисел.
+        /// Результат представить в виде:
+        ///     1) архива проекта программы для реализации монитора,
+        ///     2) скриншота результата выполнения программы.
+        ///  https://docs.microsoft.com/en-us/dotnet/api/system.threading.monitor?view=net-6.0
+        ///  https://metanit.com/sharp/tutorial/11.5.php
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-        //https://metanit.com/sharp/tutorial/11.5.php
+
+            int ProcessCount = 5;//число процессов,
+            int //количество циклов вычисления случайных чисел.
 
             Console.WriteLine("Hello World!");
         }
