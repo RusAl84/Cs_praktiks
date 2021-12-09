2c4339104bc12f1a2e9d653e2b72da8c22a1e88d
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-12-09 10:21:58 +0300

    add logic

--- Monitor/testMonitor/Program.cs
+++ Monitor/testMonitor/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace testMonitor
 {
     internal class Program
     {
+
+
+
         /// <summary>
         /// Задание
-        ///    1. Создайте консольное приложение на языке C# в среде MS Visual Studio для реализации монитора.
+        ///    1. Создайте консольное приложение на языке C# в среде MS Visual Studio 
+        ///    для реализации монитора.
         ///    2. Задайте свои параметры:
         ///       * число процессов,
         ///       * количество циклов вычисления случайных чисел.
@@ -19,11 +26,51 @@ namespace testMonitor
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            int processCount = 5;//число процессов,
+            int randomCount = 10000; //количество циклов вычисления случайных чисел.
 
-            int ProcessCount = 5;//число процессов,
-            int //количество циклов вычисления случайных чисел.
 
-            Console.WriteLine("Hello World!");
+            List<Task> tasks = new List<Task>();
+            Random rnd = new Random();
+
+            long total = 0;
+            int n = 0;
+            for (int taskCtr = 0; taskCtr < processCount; taskCtr++)
+                tasks.Add( 
+                    Task.Run(() => {
+                        int[] values = new int[randomCount];
+                        int taskTotal = 0;
+                        int taskN = 0;
+                        int ctr = 0;
+
+                    Monitor.Enter(rnd);
+                    // Generaterandom integers
+                    for (ctr = 0; ctr < randomCount; ctr++)
+                        values[ctr] = rnd.Next(0, 1001);
+                    Monitor.Exit(rnd);
+                    taskN = ctr;
+                    foreach (var value in values)
+                        taskTotal += value;
+                    Console.WriteLine("Mean for task {0,2}: {1:N2} (N={2:N0})",
+                                      Task.CurrentId, (taskTotal * 1.0) / taskN,
+                                      taskN);
+                    Interlocked.Add(ref n, taskN);
+                    Interlocked.Add(ref total, taskTotal);
+                }));
+
+
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+                Console.WriteLine("\nMean for all tasks: {0:N2} (N={1:N0})",
+                                  (total * 1.0) / n, n);
+            }
+            catch (AggregateException e)
+            {
+                foreach (var ie in e.InnerExceptions)
+                    Console.WriteLine("{0}: {1}", ie.GetType().Name, ie.Message);
+            }
+
         }
     }
 }
