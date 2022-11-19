e6cfe03ab46e7387defe636304fbd6362649e236
Aleksey <rusal@bk.ru>
2022-11-19 11:53:05 +0300

    add grath

---
+++ ATD-GRAPH/var83/var83WF/ATDGraphClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var83WF
+{
+    // var83 матрица инцидентности
+    // строки это дуги
+    // столбцы это вершины
+
+    internal class ATDGraphClass
+    {
+        private int n = 10;
+        private int m = 6;
+        private int[,] data = new int[6,10];
+
+        public ATDGraphClass() {
+
+        }
+
+        public void dataToString()
+        {
+            string textData = "";
+            string line = "";
+            for (int i=0; i <n; i++)
+            {
+                for(int j = 0; j < m-1; j++) { 
+                    line += String.Format("{0},", data[i,j]);
+                }
+                line += String.Format("{0},", data[i, m-1]);
+            }
+            textData += line + "\n";
+        }
+        //public void ADD_V(<имя>, <метка, mark>)
+    }
+}
---
+++ ATD-GRAPH/var83/var83WF/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace var83WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(407, 262);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "0, 0, 0, -1, 0, 0, 0, 0, -1, 1,0\n1, 1, 1, 1, 1, 0, 0, 0, 0, 0,1\n0, -1, 0, 0, 0, 0" +
+    ", 0, -1, 1, 0,-1\n0, 0, -1, 0, 0, 0, -1, 1, 0, 0,0\n0, 0, 0, 0, -1, 1, 0, 0, 0, -1" +
+    ",0,\n-1, 0, 0, 0, 0, -1, 1, 0, 0, 0,0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
